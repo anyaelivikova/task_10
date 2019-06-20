5209378db89e6f75c3c77bd1434afe67f3216598
Anna <Anna@DESKTOP-CF9APJJ>
2019-06-21 00:30:07 +0500

    Добавьте файлы проекта.

---
+++ Task10_533/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_533
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("<<< задача 10 >>>");
+            Console.Write("Введите число n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            TwoList twoList = new TwoList();
+            for (int i = 0; i < n; i++)
+            {
+                twoList.Add(random.Next(20));
+            }
+
+            twoList.Show();
+
+            int p = twoList.Calc();
+
+            Console.WriteLine($"Результат = {p}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task10_533/TwoList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_533
+{
+    public class TwoList
+    {
+        private class Point
+        {
+            public int Data { get; set; }
+            public Point Next { get; set; }
+            public Point Pred { get; set; }
+        }
+
+        private Point _beg;
+        private Point _last;
+
+        public void Add(int element)
+        {
+            Point p = new Point();
+            p.Data = element;
+
+            if (_beg == null)
+            {
+                _beg = p;
+                _last = p;
+            }
+            else
+            {
+                _last.Next = p;
+                p.Pred = _last;
+                _last = p;
+            }
+        }
+
+        public void Show()
+        {
+            Point p = _beg;
+            while (p != null)
+            {
+                Console.Write(p.Data + " ");
+                p = p.Next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public int Calc()
+        {
+            Point start = _beg;
+            Point end = _last;
+
+            int p = 1;
+
+            while (start != null)
+            {
+                p = p * (start.Data + end.Data);
+
+                start = start.Next;
+                end = end.Pred;
+            }
+
+            return p;
+        }
+    }
+}
